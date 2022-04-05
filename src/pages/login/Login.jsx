b6c763f13947b51768a8c05485e7d0c7427d4c25
EliasGalindo0 <eliasgalindo@gmail.com>
2022-04-05 18:22:17 -0300

    Tela de Login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import logo from '../../trivia.png';
 
 class Login extends React.Component {
@@ -12,11 +13,26 @@ class Login extends React.Component {
     };
   }
 
+  validation = () => {
+    const { name, email } = this.state;
+    const fieldMinLenght = 2;
+    const regexEmail = email.match(/[\w.!#$%&'*+=?^_`{|}~-]+@[\w.-]+\.[A-Z]{2,}/gmi);
+    const emailValidate = email.match(regexEmail);
+    this.setState({
+      btnDisable: !(emailValidate && name.length >= fieldMinLenght),
+    });
+  }
+
   handleChange = ({ target }) => {
     const { name, value } = target;
     this.setState({
       [name]: value,
-    });
+    }, () => this.validation());
+  }
+
+  handleClick = () => {
+    const { history } = this.props;
+    history.push('/');
   }
 
   render() {
@@ -29,28 +45,42 @@ class Login extends React.Component {
           <p>
             SUA VEZ
           </p>
+          <div className="inputs-box">
+            <input
+              type="email"
+              name="email"
+              value={ email }
+              onChange={ this.handleChange }
+              data-testid="input-gravatar-email"
+            />
+
+            <input
+              type="text"
+              name="name"
+              value={ name }
+              onChange={ this.handleChange }
+              data-testid="input-player-name"
+            />
+            <button
+              type="button"
+              data-testid="btn-play"
+              name="btn-play"
+              onClick={ this.handleClick }
+              disabled={ btnDisable }
+            >
+              Play
+            </button>
+          </div>
         </header>
-
-        <div className="inputs-box">
-          <input
-            type="email"
-            name="email"
-            value={ email }
-            onChange={ this.handleChange }
-            data-testid="input-gravatar-email"
-          />
-
-          <input
-            type="text"
-            name="name"
-            value={ name }
-            onChange={ this.handleChange }
-            data-testid="input-player-name"
-          />
-        </div>
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Login;
