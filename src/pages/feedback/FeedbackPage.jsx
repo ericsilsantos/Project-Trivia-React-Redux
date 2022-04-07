1ff9a2d9e96853e560612eb18397defebda15271
EliasGalindo0 <eliasgalindo@gmail.com>
2022-04-07 17:30:29 -0300

    Cria Pagina de feedback com header

---
+++ src/pages/feedback/FeedbackPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Feedback from '../../components/Feedback';
+import Header from '../../components/Header';
+
+class FeedbackPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Feedback />
+      </div>
+    );
+  }
+}
+
+export default FeedbackPage;
